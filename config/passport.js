49b1be6ac5f5e4605e2999094541eba321404032
sonnegod <bencerbins@gmail.com>
2025-10-14 23:50:44 -0400

    More

--- config/passport.js
+++ config/passport.js
@@ -10,8 +10,8 @@ const STEAM_API_KEY = process.env.STEAM_API_KEY; // Replace with your real API k
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((obj, done) => done(null, obj));
 
-const returnURL = '';
-const realmURL = '';
+let returnURL = '';
+let realmURL = '';
 if(process.env.ENVIRONMENT === 'DEV'){
   returnURL = `http://localhost:${process.env.SERVER_PORT}/api/auth/steam/return`;
   realmURL = `http://localhost:${process.env.SERVER_PORT}/`;
