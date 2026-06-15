85af720306053956d18053af33137f5f537e70ba
Ben Cerbins <bencerbins@gmail.com>
2026-06-15 14:58:07 -0500

    fix for match page table

--- client/src/pages/MatchPage.jsx
+++ client/src/pages/MatchPage.jsx
@@ -79,7 +79,9 @@ export default function MatchPage() {
               {radiantPlayers.map(p => (
                 <tr key={p.PlayerId}>
                   <td style={thTdStyle}><Link to={`/player/${p.PlayerId}`}>{p.PlayerName}</Link></td>
-                  <td style={thTdStyle}><HeroDisplay heroId={p.HeroId} heroName={p.HeroName} /></td>
+                  <td style={thTdStyle}>
+                    <HeroDisplay heroId={p.HeroId} heroName={p.HeroName} showName={false} />
+                  </td>
                   <td style={thTdStyle}>{p.Kills}</td>
                   <td style={thTdStyle}>{p.Deaths}</td>
                   <td style={thTdStyle}>{p.Assists}</td>
@@ -120,7 +122,9 @@ export default function MatchPage() {
               {direPlayers.map(p => (
                 <tr key={p.PlayerId}>
                   <td style={thTdStyle}><Link to={`/player/${p.PlayerId}`}>{p.PlayerName}</Link></td>
-                  <td style={thTdStyle}><HeroDisplay heroId={p.HeroId} heroName={p.HeroName} /></td>
+                  <td style={thTdStyle}>
+                    <HeroDisplay heroId={p.HeroId} heroName={p.HeroName} showName={false} />
+                  </td>
                   <td style={thTdStyle}>{p.Kills}</td>
                   <td style={thTdStyle}>{p.Deaths}</td>
                   <td style={thTdStyle}>{p.Assists}</td>
@@ -167,7 +171,7 @@ export default function MatchPage() {
                           </td>
                           <td style={thTdStyle}>{pb.IsPick ? 'Pick' : 'Ban'}</td>
                           <td style={thTdStyle}>
-                            <HeroDisplay heroId={pb.HeroId} heroName={pb.HeroName} iconSize={40} />
+                            <HeroDisplay heroId={pb.HeroId} heroName={pb.HeroName} />
                           </td>
                         </tr>
                       ))}
